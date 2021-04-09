d796641754294a97b313db670cc2771117fd6fa4
Omar Polo <op@omarpolo.com>
2021-04-09 19:27:14 +0200

    sync lua bindings

--- examples/lua/phos.c
+++ examples/lua/phos.c
@@ -18,6 +18,7 @@
 
 #include <errno.h>
 #include <phos.h>
+#include <string.h>
 
 #include <lua.h>
 
@@ -25,21 +26,54 @@
 #include <lualib.h>
 
 static int lp_client_new(lua_State*);
+static int lp_server_new(lua_State*);
 
 static int lp_client__tostring(lua_State*);
 static int lp_client__gc(lua_State*);
 static int lp_client_req(lua_State*);
-static int lp_client_run(lua_State*);
-static int lp_client_run_sync(lua_State*);
-static int lp_client_fd(lua_State*);
-static int lp_client_state(lua_State*);
-static int lp_client_res(lua_State*);
-static int lp_client_buf(lua_State*);
-static int lp_client_abort(lua_State*);
+static int lp_client_handshake(lua_State*);
+static int lp_client_handshake_sync(lua_State*);
+static int lp_client_response(lua_State*);
+static int lp_client_response_sync(lua_State*);
+static int lp_client_read(lua_State*);
+static int lp_client_read_sync(lua_State*);
 static int lp_client_close(lua_State*);
+static int lp_client_close_sync(lua_State*);
+static int lp_client_abort(lua_State*);
+static int lp_client_abort_sync(lua_State*);
+static int lp_client_fd(lua_State*);
+static int lp_client_res(lua_State*);
+static int lp_client_err(lua_State*);
+
+static int lp_server__tostring(lua_State*);
+static int lp_server__gc(lua_State*);
+static int lp_server_load_kp(lua_State*);
+static int lp_server_accept(lua_State*);
+static int lp_server_accept_sync(lua_State*);
+static int lp_server_accept_fd(lua_State*);
+static int lp_server_fd(lua_State*);
+static int lp_server_err(lua_State*);
+
+static int lp_req__tostring(lua_State*);
+static int lp_req__gc(lua_State*);
+static int lp_req_handshake(lua_State*);
+static int lp_req_handshake_sync(lua_State*);
+static int lp_req_read_request(lua_State*);
+static int lp_req_read_request_sync(lua_State*);
+static int lp_req_reply(lua_State*);
+static int lp_req_reply_flush(lua_State*);
+static int lp_req_reply_sync(lua_State*);
+static int lp_req_write(lua_State*);
+static int lp_req_write_sync(lua_State*);
+static int lp_req_close(lua_State*);
+static int lp_req_close_sync(lua_State*);
+static int lp_req_fd(lua_State*);
+static int lp_req_line(lua_State*);
+static int lp_req_err(lua_State*);
 
 static const luaL_Reg phoslib[] = {
 	{ "new_client",		lp_client_new },
+	{ "new_server",		lp_server_new },
 	/* placeholders */
 	{ "want_read",		NULL },
 	{ "want_write",		NULL },
@@ -50,27 +84,51 @@ static const struct luaL_Reg client_m[] = {
 	{ "__tostring",		lp_client__tostring},
 	{ "__gc",		lp_client__gc},
 	{ "req",		lp_client_req },
-	{ "run",		lp_client_run },
-	{ "run_sync",		lp_client_run_sync },
-	{ "fd",			lp_client_fd },
-	{ "state",		lp_client_state },
-	{ "res",		lp_client_res },
-	{ "buf",		lp_client_buf },
+	{ "handshake",		lp_client_handshake },
+	{ "handshake_sync",	lp_client_handshake_sync },
+	{ "response",		lp_client_response },
+	{ "response_sync",	lp_client_response_sync },
+	{ "read",		lp_client_read },
+	{ "read_sync",		lp_client_read_sync },
 	{ "abort",		lp_client_abort },
+	{ "abort_sync",		lp_client_abort_sync },
 	{ "close",		lp_client_close },
-	/* placeholders */
-	{ "s_start",		NULL },
-	{ "s_resolution",	NULL },
-	{ "s_connect",		NULL },
-	{ "s_handshake",	NULL },
-	{ "s_post_handshake",	NULL },
-	{ "s_writing_req",	NULL },
-	{ "s_reading_header",	NULL },
-	{ "s_reply_ready",	NULL },
-	{ "s_body",		NULL },
-	{ "s_closing",		NULL },
-	{ "s_eof",		NULL },
-	{ "s_error",		NULL },
+	{ "close_sync",		lp_client_close_sync },
+	{ "fd",			lp_client_fd },
+	{ "res",		lp_client_res },
+	{ "err",		lp_client_err },
+	{ NULL, NULL}
+};
+
+static const struct luaL_Reg server_m[] = {
+	{ "__tostring",		lp_server__tostring},
+	{ "__gc",		lp_server__gc},
+	{ "load_keypair_file",	lp_server_load_kp },
+	{ "accept",		lp_server_accept },
+	{ "accept_fd",		lp_server_accept_fd },
+	{ "accept_sync",	lp_server_accept_sync },
+	{ "fd",			lp_server_fd },
+	{ "err",		lp_server_err },
+	{ NULL, NULL}
+};
+
+static const struct luaL_Reg req_m[] = {
+	{ "__tostring",		lp_req__tostring},
+	{ "__gc",		lp_req__gc},
+	{ "handhsake",		lp_req_handshake },
+	{ "handhsake_sync",	lp_req_handshake_sync },
+	{ "read_request",	lp_req_read_request },
+	{ "read_request_sync",	lp_req_read_request_sync },
+	{ "reply",		lp_req_reply },
+	{ "reply_flush",	lp_req_reply_flush },
+	{ "reply_sync",		lp_req_reply_sync },
+	{ "write",		lp_req_write },
+	{ "write_sync",		lp_req_write_sync },
+	{ "close",		lp_req_close },
+	{ "close_sync",		lp_req_close_sync },
+	{ "fd",			lp_req_fd },
+	{ "line",		lp_req_line },
+	{ "err",		lp_req_err },
 	{ NULL, NULL}
 };
 
@@ -88,35 +146,51 @@ luaopen_phos(lua_State *L)
 	lua_pushvalue(L, -1);
 	lua_setfield(L, -2, "__index");
 	luaL_setfuncs(L, client_m, 0);
+	lua_pop(L, 1);
 
-	lua_pushnumber(L, PCS_START);
-	lua_setfield(L, -2, "s_start");
-	lua_pushnumber(L, PCS_RESOLUTION);
-	lua_setfield(L, -2, "s_resolution");
-	lua_pushnumber(L, PCS_CONNECT);
-	lua_setfield(L, -2, "s_connect");
-	lua_pushnumber(L, PCS_HANDSHAKE);
-	lua_setfield(L, -2, "s_handshake");
-	lua_pushnumber(L, PCS_POST_HANDSHAKE);
-	lua_setfield(L, -2, "s_post_handshake");
-	lua_pushnumber(L, PCS_WRITING_REQ);
-	lua_setfield(L, -2, "s_writing_req");
-	lua_pushnumber(L, PCS_READING_HEADER);
-	lua_setfield(L, -2, "s_reading_header");
-	lua_pushnumber(L, PCS_REPLY_READY);
-	lua_setfield(L, -2, "s_reply_ready");
-	lua_pushnumber(L, PCS_BODY);
-	lua_setfield(L, -2, "s_body");
-	lua_pushnumber(L, PCS_CLOSING);
-	lua_setfield(L, -2, "s_closing");
-	lua_pushnumber(L, PCS_EOF);
-	lua_setfield(L, -2, "s_eof");
-	lua_pushnumber(L, PCS_ERROR);
-	lua_setfield(L, -2, "s_error");
+	luaL_newmetatable(L, "phos.server");
+	lua_pushvalue(L, -1);
+	lua_setfield(L, -2, "__index");
+	luaL_setfuncs(L, server_m, 0);
+	lua_pop(L, 1);
 
+	luaL_newmetatable(L, "phos.req");
+	lua_pushvalue(L, -1);
+	lua_setfield(L, -2, "__index");
+	luaL_setfuncs(L, req_m, 0);
 	lua_pop(L, 1);
 
 	return 1;
+}
+
+static inline struct phos_client *
+checkclient(lua_State *L)
+{
+	struct phos_client *client;
+
+	client = luaL_checkudata(L, 1, "phos.client");
+	luaL_argcheck(L, client != NULL, 1, "`client` expected");
+	return client;
+}
+
+static inline struct phos_server *
+checkserver(lua_State *L)
+{
+	struct phos_server *server;
+
+	server = luaL_checkudata(L, 1, "phos.server");
+	luaL_argcheck(L, server != NULL, 1, "`server` expected");
+	return server;
+}
+
+static inline struct phos_req *
+checkreq(lua_State *L)
+{
+	struct phos_req *req;
+
+	req = luaL_checkudata(L, 1, "phos.req");
+	luaL_argcheck(L, req != NULL, 1, "`req` expected");
+	return req;
 }
 
 static int
@@ -140,14 +214,29 @@ lp_client_new(lua_State *L)
 	return 1;
 }
 
-static struct phos_client *
-checkclient(lua_State *L)
+static int
+lp_server_new(lua_State *L)
 {
-	struct phos_client *client;
+	struct phos_server	*serv;
+	const char		*host, *port;
 
-	client = luaL_checkudata(L, 1, "phos.client");
-	luaL_argcheck(L, client != NULL, 1, "`client` expected");
-	return client;
+	if ((serv = lua_newuserdata(L, sizeof(*serv))) == NULL) {
+		/* probably not needed? */
+		lua_pushnil(L);
+		return 1;
+	}
+
+	host = luaL_checkstring(L, 2);
+	port = luaL_checkstring(L, 3);
+
+	luaL_argcheck(L, phos_server_init(serv, host, port) != -1, 1,
+	    "phos_server_init failed");
+
+	luaL_getmetatable(L, "phos.server");
+	lua_setmetatable(L, -2);
+
+	/* the serv is already on the stack */
+	return 1;
 }
 
 static int
@@ -185,38 +274,111 @@ lp_client_req(lua_State *L)
 }
 
 static int
-lp_client_run(lua_State *L)
+lp_client_handshake(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_handshake(checkclient(L)));
+	return 1;
+}
+
+static int
+lp_client_handshake_sync(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_handshake_sync(checkclient(L)));
+	return 1;
+}
+
+static int
+lp_client_response(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_response(checkclient(L)));
+	return 1;
+}
+
+static int
+lp_client_response_sync(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_response_sync(checkclient(L)));
+	return 1;
+}
+
+static int
+lp_client_read(lua_State *L)
 {
 	struct phos_client	*client;
+	ssize_t			 r;
+	char			 buf[1024];
 
 	client = checkclient(L);
-	lua_pushinteger(L, phos_client_run(client));
-	lua_pushinteger(L, phos_client_state(client));
+	switch (r = phos_client_read(client, buf, sizeof(buf))) {
+	case PHOS_WANT_READ:
+	case PHOS_WANT_WRITE:
+	case 0:
+	case -1:
+		lua_pushinteger(L, r);
+		lua_pushstring(L, "");
+		break;
+	default:
+		lua_pushinteger(L, (int)r);
+		lua_pushlstring(L, buf, (size_t)r);
+		break;
+	}
 	return 2;
 }
 
 static int
-lp_client_run_sync(lua_State *L)
+lp_client_read_sync(lua_State *L)
 {
 	struct phos_client	*client;
+	ssize_t			 r;
+	char			 buf[1024];
 
 	client = checkclient(L);
-	lua_pushinteger(L, phos_client_run_sync(client));
-	lua_pushinteger(L, phos_client_state(client));
+	switch (r = phos_client_read_sync(client, buf, sizeof(buf))) {
+	case 0:
+	case -1:
+		lua_pushstring(L, "");
+		lua_pushinteger(L, r);
+		break;
+	default:
+		lua_pushlstring(L, buf, (size_t)r);
+		lua_pushinteger(L, (int)r);
+		break;
+	}
 	return 2;
+}
+
+static int
+lp_client_close(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_close(checkclient(L)));
+	return 1;
+}
+
+static int
+lp_client_close_sync(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_close_sync(checkclient(L)));
+	return 1;
+}
+
+static int
+lp_client_abort(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_abort(checkclient(L)));
+	return 1;
+}
+
+static int
+lp_client_abort_sync(lua_State *L)
+{
+	lua_pushinteger(L, phos_client_abort_sync(checkclient(L)));
+	return 1;
 }
 
 static int
 lp_client_fd(lua_State *L)
 {
 	lua_pushinteger(L, phos_client_fd(checkclient(L)));
-	return 1;
-}
-
-static int
-lp_client_state(lua_State *L)
-{
-	lua_pushinteger(L, phos_client_state(checkclient(L)));
 	return 1;
 }
 
@@ -233,30 +395,280 @@ lp_client_res(lua_State *L)
 }
 
 static int
-lp_client_buf(lua_State *L)
+lp_client_err(lua_State *L)
 {
-	struct phos_client *client;
-
-	client = checkclient(L);
-
-	lua_pushlstring(L, client->buf, client->off);
+	lua_pushstring(L, checkclient(L)->err);
 	return 1;
 }
 
 static int
-lp_client_abort(lua_State *L)
+lp_server__tostring(lua_State *L)
 {
-	int r;
-
-	r = phos_client_abort(checkclient(L)) != -1;
-	lua_pushboolean(L, r);
+	lua_pushstring(L, "phos-server");
 	return 1;
 }
 
 static int
-lp_client_close(lua_State *L)
+lp_server__gc(lua_State *L)
 {
-	phos_client_close(checkclient(L));
-	lua_pushnil(L);
+	phos_server_del(checkserver(L));
+	return 0;
+}
+
+static int
+lp_server_load_kp(lua_State *L)
+{
+	struct phos_server	*serv;
+	int			 r;
+	const char		*cert;
+	const char		*key;
+
+	serv = checkserver(L);
+	cert = luaL_checkstring(L, 2);
+	key  = luaL_checkstring(L, 3);
+
+	r = phos_server_load_keypair_file(serv, cert, key);
+	lua_pushboolean(L, r != -1);
+	return 1;
+}
+
+static int
+lp_server_accept(lua_State *L)
+{
+	struct phos_server	*serv;
+	struct phos_req		 req, *luareq;
+	int			 r;
+
+	serv = checkserver(L);
+	switch (r = phos_server_accept(serv, &req)) {
+	case 0:
+		lua_pushinteger(L, 0);
+		if ((luareq = lua_newuserdata(L, sizeof(*luareq))) == NULL) {
+			lua_pop(L, 1);
+			lua_pushinteger(L, -1);
+			lua_pushnil(L);
+			return 2;
+		}
+
+		memcpy(luareq, &req, sizeof(req));
+		luaL_getmetatable(L, "phos.req");
+		lua_setmetatable(L, -2);
+		return 2;
+	default:
+		lua_pushinteger(L, r);
+		lua_pushnil(L);
+		return 2;
+	}
+}
+
+static int
+lp_server_accept_sync(lua_State *L)
+{
+	struct phos_server	*serv;
+	struct phos_req		 req, *luareq;
+	int			 r;
+
+	serv = checkserver(L);
+	switch (r = phos_server_accept_sync(serv, &req)) {
+	case 0:
+		lua_pushinteger(L, 0);
+		if ((luareq = lua_newuserdata(L, sizeof(*luareq))) == NULL) {
+			lua_pop(L, 1);
+			lua_pushinteger(L, -1);
+			lua_pushnil(L);
+			return 2;
+		}
+
+		memcpy(luareq, &req, sizeof(req));
+		luaL_getmetatable(L, "phos.req");
+		lua_setmetatable(L, -2);
+		return 2;
+	default:
+		lua_pushinteger(L, r);
+		lua_pushnil(L);
+		return 2;
+	}
+}
+
+static int
+lp_server_accept_fd(lua_State *L)
+{
+	struct phos_server	*serv;
+	struct phos_req		 req, *luareq;
+	int			 fd, r;
+
+	serv = checkserver(L);
+	fd = luaL_checkinteger(L, 2);
+
+	switch (r = phos_server_accept_fd(serv, &req, fd)) {
+	case 0:
+		lua_pushinteger(L, 0);
+		if ((luareq = lua_newuserdata(L, sizeof(*luareq))) == NULL) {
+			lua_pop(L, 1);
+			lua_pushinteger(L, -1);
+			lua_pushnil(L);
+			return 2;
+		}
+
+		memcpy(luareq, &req, sizeof(req));
+		luaL_getmetatable(L, "phos.req");
+		lua_setmetatable(L, -2);
+		return 2;
+	default:
+		lua_pushinteger(L, r);
+		lua_pushnil(L);
+		return 2;
+	}
+}
+
+static int
+lp_server_fd(lua_State *L)
+{
+	lua_pushinteger(L, checkserver(L)->fd);
+	return 1;
+}
+
+static int
+lp_server_err(lua_State *L)
+{
+	lua_pushstring(L, checkserver(L)->err);
+	return 1;
+}
+
+static int
+lp_req__tostring(lua_State *L)
+{
+	lua_pushstring(L, "phos-req");
+	return 1;
+}
+
+static int
+lp_req__gc(lua_State *L)
+{
+	phos_req_del(checkreq(L));
+	return 0;
+}
+
+static int
+lp_req_handshake(lua_State *L)
+{
+	lua_pushinteger(L, phos_req_handshake(checkreq(L)));
+	return 1;
+}
+
+static int
+lp_req_handshake_sync(lua_State *L)
+{
+	lua_pushinteger(L, phos_req_handshake_sync(checkreq(L)));
+	return 1;
+}
+
+static int
+lp_req_read_request(lua_State *L)
+{
+	lua_pushinteger(L, phos_req_read_request(checkreq(L)));
+	return 1;
+}
+
+static int
+lp_req_read_request_sync(lua_State *L)
+{
+	lua_pushinteger(L, phos_req_read_request_sync(checkreq(L)));
+	return 1;
+}
+
+static int
+lp_req_reply(lua_State *L)
+{
+	struct phos_req	*req;
+	int		 code;
+	const char	*meta;
+
+	req  = checkreq(L);
+	code = luaL_checkinteger(L, 2);
+	meta = luaL_checkstring(L, 3);
+
+	lua_pushinteger(L, phos_req_reply(req, code, meta));
+	return 1;
+}
+
+static int
+lp_req_reply_flush(lua_State *L)
+{
+	lua_pushinteger(L, phos_req_reply_flush(checkreq(L)));
+	return 1;
+}
+
+static int
+lp_req_reply_sync(lua_State *L)
+{
+	struct phos_req	*req;
+	int		 code;
+	const char	*meta;
+
+	req  = checkreq(L);
+	code = luaL_checkinteger(L, 2);
+	meta = luaL_checkstring(L, 3);
+
+	lua_pushinteger(L, phos_req_reply_sync(req, code, meta));
+	return 1;
+}
+
+static int
+lp_req_write(lua_State *L)
+{
+	struct phos_req	*req;
+	const char	*buf;
+
+	req = checkreq(L);
+	buf = luaL_checkstring(L, 2);
+	lua_pushinteger(L, phos_req_write(req, buf, strlen(buf)));
+	return 1;
+}
+
+static int
+lp_req_write_sync(lua_State *L)
+{
+	struct phos_req	*req;
+	const char	*buf;
+
+	req = checkreq(L);
+	buf = luaL_checkstring(L, 2);
+	lua_pushinteger(L, phos_req_write_sync(req, buf, strlen(buf)));
+	return 1;
+}
+
+static int
+lp_req_close(lua_State *L)
+{
+	lua_pushinteger(L, phos_req_close(checkreq(L)));
+	return 1;
+}
+
+static int
+lp_req_close_sync(lua_State *L)
+{
+	lua_pushinteger(L, phos_req_close_sync(checkreq(L)));
+	return 1;
+}
+
+static int
+lp_req_fd(lua_State *L)
+{
+	lua_pushinteger(L, checkreq(L)->fd);
+	return 1;
+}
+
+static int
+lp_req_line(lua_State *L)
+{
+	lua_pushstring(L, checkreq(L)->line);
+	return 1;
+}
+
+static int
+lp_req_err(lua_State *L)
+{
+	lua_pushstring(L, checkreq(L)->err);
 	return 1;
 }
