3ff795e955ae1da295a1fbb8865937f0e9c9ddc3
Omar Polo <op@omarpolo.com>
2021-04-13 10:42:06 +0200

    fix path; ignore afl data; use afl-cmin

--- gen-afl-data.sh
+++ gen-afl-data.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -e
+
 i=0
 
 grep -e '[^e].URI(' tests/uri_parse_test.c | \
 	awk -F '"' '{ print $2 }' | \
 	while read line; do
-		printf "%s\n" "$line" > afl/in/${i}
+		printf "%s\n" "$line" > afl/in.orig/${i}
 		i=$((i+1))
 	done
+
+afl-cmin -i afl/in.orig -o afl/in -- ./fuzzy_uri_parser
